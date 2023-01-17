6c4af505e676d6afccc637bb3d02ed8574cb4dc9
Elena Rentero <elenaclementerentero@gmail.com>
2023-01-17 14:30:26 +0100

    Add code first class

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Aprendiendo React ⚛️
 Curso para aprender React basado en proyectos impartido por [midudev](https://github.com/midudev).
 ## Proyectos
+- 01: [Twitter Follow Card](projects/01-twitter-follow-card/)
---
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -0,0 +1,36 @@
+import "./App.css";
+import { TwitterFollowCard } from "./TwitterFollowCard";
+
+const users = [
+  {
+    userName: "ElenaRentero",
+    name: "Elena Clemente Rentero",
+    isFollowing: true,
+  },
+  {
+    userName: "AlvaroFaure",
+    name: "Álvaro Faure",
+    isFollowing: true,
+  },
+  {
+    userName: "Adalab",
+    name: "Adalab",
+    isFollowing: false,
+  },
+];
+
+export function App() {
+  return (
+    <section className="App">
+      {users.map(({ userName, name, isFollowing }) => (
+        <TwitterFollowCard
+          key={userName}
+          userName={userName}
+          initialIsFollowing={isFollowing}
+        >
+          {name}
+        </TwitterFollowCard>
+      ))}
+    </section>
+  );
+}
